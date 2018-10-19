c6a605c64ef3afd6ea976517e8ad76a3004cb82f
Patricio Palladino <email@patriciopalladino.com>
2018-10-19 12:57:32 -0300

    Fix the unlock metamask button

--- ui/components/UnlockMetaMaskMessage.js
+++ ui/components/UnlockMetaMaskMessage.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function UnlockMetaMaskMessage() {
-  return (
-    <div className="overlay-message">
-      <div>
-        <h2>Please unlock MetaMask to continue</h2>
+export default class UnlockMetaMaskMessage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showUnlockButton: false
+    };
+
+    if (window.ethereum) {
+      if (window.ethereum.isMetaMask) {
+        if (window.ethereum.isApproved) {
+          window.ethereum
+            .isApproved()
+            .then(v => this.setState({ showUnlockButton: !v }));
+        } else {
+          this.state.showUnlockButton = false;
+        }
+      } else {
+        this.state.showUnlockButton = true;
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="overlay-message">
         <div>
-          <br />
-          <br />
-          <br />
-          {window.ethereum && (
-            <button
-              className="btn btn-primary"
-              onClick={() => window.ethereum.enable()}
-            >
-              Unlock
-            </button>
-          )}
+          <h2>Please unlock MetaMask to continue</h2>
+          <div>
+            <br />
+            <br />
+            <br />
+            {this.state.showUnlockButton && (
+              <button
+                className="btn btn-primary"
+                onClick={() => window.ethereum.enable()}
+              >
+                Unlock
+              </button>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
