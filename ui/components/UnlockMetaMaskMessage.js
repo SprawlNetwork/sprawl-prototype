b37b555ddae2534964ace2646e64bc61b4abaf43
Patricio Palladino <email@patriciopalladino.com>
2018-10-19 15:29:33 -0300

    Simplify Metamask unlocking

--- ui/components/UnlockMetaMaskMessage.js
+++ ui/components/UnlockMetaMaskMessage.js
@@ -1,26 +1,8 @@
 import React, { Component } from "react";
 
 export default class UnlockMetaMaskMessage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showUnlockButton: false
-    };
-
-    if (window.ethereum) {
-      if (window.ethereum.isMetaMask) {
-        if (window.ethereum.isApproved) {
-          window.ethereum
-            .isApproved()
-            .then(v => this.setState({ showUnlockButton: !v }));
-        } else {
-          this.state.showUnlockButton = false;
-        }
-      } else {
-        this.state.showUnlockButton = true;
-      }
-    }
+  componentDidMount() {
+    window.ethereum.enable();
   }
 
   render() {
@@ -28,19 +10,6 @@ export default class UnlockMetaMaskMessage extends Component {
       <div className="overlay-message">
         <div>
           <h2>Please unlock MetaMask to continue</h2>
-          <div>
-            <br />
-            <br />
-            <br />
-            {this.state.showUnlockButton && (
-              <button
-                className="btn btn-primary"
-                onClick={() => window.ethereum.enable()}
-              >
-                Unlock
-              </button>
-            )}
-          </div>
         </div>
       </div>
     );
